45433c9bbf461aeddf03b1013403e33abca8159e
abelinaaulia <abelinaaulia@gmail.com>
2021-12-24 10:50:08 -0800

    nambahkan file baru

--- routes/web.php
+++ routes/web.php
@@ -22,22 +22,41 @@ Auth::routes([
 ]);
 
 //hanya untuk role admin
-Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], function(){
-    Route::get('/', function(){
-        return 'halaman admin';
- });
-   Route::get('profile', function(){
-       return 'halaman profile admin';
-   });
-});
+//Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], function(){
+  //  Route::get('/', function(){
+   //     return 'halaman admin';
+ //});
+  // Route::get('profile', function(){
+     //  return 'halaman profile admin';
+   //});
+//});
 
 //hanya untuk role pengguna
-Route::group(['prefix' => 'pengguna', 'middleware' => ['auth', 'role:pengguna']], function(){
-    Route::get('/', function(){
-        return 'halaman pengguna';
-    });
-    Route::get('profile', function(){
-        return 'halaman profile pengguna';
-    });
+//Route::group(['prefix' => 'pengguna', 'middleware' => ['auth', 'role:pengguna']], function(){
+   // Route::get('/', function(){
+     //   return 'halaman pengguna';
+   // });
+    //Route::get('profile', function(){
+    //    return 'halaman profile pengguna';
+  //  });
+//});
+
+Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function(){
+      Route::get('buku', function(){
+        return view ('buku.index');
+   });
+Route::get('pengarang', function(){
+    return view('pengarang.index');
+});
+});
+
+Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function(){
+    Route::get('buku', function(){
+      return view ('buku.index');
+ })->middleware(['role:admin|pengguna']);
+
+    Route::get('pengarang', function(){
+      return view ('pengarang.index');
+ })->middleware(['role:admin']);
 });
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
